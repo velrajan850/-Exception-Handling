5666cfb715a5d05b82dfd5b85316a176897efaf9
velrajan850 <105338994+velrajan850@users.noreply.github.com>
2022-05-16 17:37:04 +0530

    Add files via upload

---
+++ excedemo.java
@@ -0,0 +1,25 @@
+package gits;
+
+public class excedemo {
+	
+
+	public static void main(String[] args) {
+		int a =0;
+		int b=10;
+		
+		// TODO Auto-generated method stub
+		
+		try {
+			int c=b/a;
+			{
+				System.out.print("give the result");
+			}
+		}
+			catch (ArithmeticException e) {
+			System.out.print ("you shoud not divide by zero");
+			}
+		}
+
+	}
+
+
---
+++ exception.java
@@ -0,0 +1,20 @@
+package gits;
+
+public class exception {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int[] mynumbers = {1,2,3};
+		System.out.println(mynumbers[10] );}
+	catch (Exception e)
+		{
+	System.out.println(e);
+
+		}
+	}
+	}
+
+
